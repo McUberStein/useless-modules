6ba64563793a05bd55d3185aa8bc7cf112c033de
McUberStein <95429589+McUberStein@users.noreply.github.com>
2022-04-03 17:43:00 +0000

    Update main.sh

--- bad-apple/main.sh
+++ bad-apple/main.sh
@@ -1,10 +1,12 @@
-sudo pip install youtube-dl 
-if ! command -v mpv &> /dev/null
+#!/bin/bash
+# A script to install pip, download youtube-dl and download the Bad Apple video. 
+sudo pip install youtube-dl # Download pip system-wide
+if ! command -v mpv &> /dev/null # Check if mpv is installed
 then
     echo "MPV could not be found!"
     exit
 fi
 
-mkdir ~/.ascii
-youtube-dl https://www.youtube.com/watch?v=FtutLA63Cp8 -o badapple && mv badapple.mkv ~/.ascii/ 
+mkdir ~/.ascii # Make a directory to store the video
+youtube-dl https://www.youtube.com/watch?v=FtutLA63Cp8 -o badapple && mv badapple.mkv ~/.ascii/ # Download video from youtube and move it to directory
 
