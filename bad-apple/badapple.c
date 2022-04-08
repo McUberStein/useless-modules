b8fb4913ef89bf624133395ae9bba9304ee83c78
McUberStein <95429589+McUberStein@users.noreply.github.com>
2022-04-09 00:02:08 +0200

    Update badapple.c

--- bad-apple/badapple.c
+++ bad-apple/badapple.c
@@ -19,7 +19,7 @@ int init_module(void)
 void cleanup_module(void)
 {
 	printk(KERN_INFO "Bad Apple unloaded \n");
-	char *argv[] = { "/bin/bash", "-c", "/bin/killall mpv && /bin/killall mpv && clear > /dev/pts/1 && echo 'Bad Apple stopped. Press enter...' > /dev/pts/0", NULL };
+	char *argv[] = { "/bin/bash", "-c", "/bin/killall mpv && /bin/killall mpv && clear > /dev/pts/1 && echo 'Bad Apple stopped. Press enter...' > /dev/pts/1", NULL };
         static char* envp[] = { "HOME=/",  "TERM=linux",   "PATH=/sbin:/bin:/usr/sbin:/usr/bin", NULL};
         call_usermodehelper(argv[0], argv, envp, UMH_WAIT_EXEC);	
 }
